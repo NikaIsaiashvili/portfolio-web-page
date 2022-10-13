f2b2e58a786fbceace88f428a2cdf2e33bbb2ca4
NikaIsaiashvili <nisaiashvili@gmail.com>
2022-10-13 17:39:46 +0400

    full responsive update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import * as styles from "./App.module.css"
-import Header from "./components/Header/Header";
-import Body from "./components/Body/Body";
 import RoutesPath from "./Routes";
 
 function App() {
