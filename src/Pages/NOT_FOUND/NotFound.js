7d3bbc437fcc6a3c40266cc333696afe1cd71734
NikaIsaiashvili <nisaiashvili@gmail.com>
2022-10-14 16:10:53 +0400

    portfolio update
    not found page route done back to home

--- src/Pages/NOT_FOUND/NotFound.js
+++ src/Pages/NOT_FOUND/NotFound.js
@@ -1,10 +1,18 @@
+import HomePg from "../Home_PG/HomePage";
 import * as styles from "./NotFound.module.css";
+import ROUTES from "../../config/routes";
+import { NavLink } from "react-router-dom";
 
 function NotFound() {
   return (
     <div className={styles.PAGE_NOT_FOUND}>
       <h1 className={styles.Not_Found_Text}>
-        PAGE NOT FOUND <span>:(</span>
+        PAGE NOT FOUND <span>:(</span> <br/> <br/>
+        Please go back to <span><NavLink
+          to={ROUTES.HOME}
+        >
+           Home
+        </NavLink> </span>
       </h1>
     </div>
   );
