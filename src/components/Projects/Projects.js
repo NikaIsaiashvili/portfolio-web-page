e4859200226361a87c26f0214dea7d9af4a1e44e
NikaIsaiashvili <nisaiashvili@gmail.com>
2022-10-22 03:02:01 +0400

    update projects page
    added box shadow container, images container, slider buttons, project data etc

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,28 @@
+import React from "react";
+import * as styles from "../Projects/Projects.module.css";
+import projectData from "../../config/projectData";
 
 function Projects() {
-    return (
-        <>
-        <div>PROJECTS</div>
-        </>
-    )
+  return (
+    <>
+      <section className={styles.main_container}>
+        <div className={styles.slide_container}>
+          <div className={styles.left_arrow}>
+          <ion-icon name="caret-back"></ion-icon>
+          </div>
+          <div className={styles.right_arrow}>
+          <ion-icon name="caret-forward"></ion-icon>
+          </div>
+
+          {projectData.map((projects) => {
+            return (
+              <img src={projects.photo} className={styles.slide_pic}></img>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
---
+++ src/config/projectData.js
@@ -0,0 +1,24 @@
+import img1 from "../../public/images/img1.jpg";
+import img2 from "../../public/images/img2.jpg";
+import img3 from "../../public/images/img3.jpg";
+
+
+const projectData = [
+ {
+    photo: img1,
+    numb: "first photo",
+    key: "first"
+ },
+ {
+    photo: img2,
+    numb: "second photo",
+    key: "second"
+ },
+ {
+    photo: img3,
+    numb: "third photo",
+    key: "third"
+ },
+];
+
+export default projectData;
